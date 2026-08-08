b6a7053316f0bd4002373f7ac39d5dbf09b24ced
andrluiz125 <andrluiz125@gmail.com>
2026-08-07 22:23:00 -0300

    Aplica novo espaçamento da seção manifesto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Fraunces, Inter } from "next/font/google";
 import "./globals.css";
 import "./marquee.css";
+import "./manifesto-spacing.css";
 
 const inter = Inter({
   variable: "--font-body",
